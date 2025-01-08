9aea8c5ac2cd756f77b30b86a7da38477a3386bf
Leticiakhrn <leticiakharouni06@gmail.com>
2025-01-08 19:12:32 +0100

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,30 @@
-//
+import React, { useState } from 'react';
+import HomePage from './pages/HomePage';
+import ListPage from './pages/ListPage';
+import DetailPage from './pages/DetailPage';
+
+function App() {
+  const [currentPage, setCurrentPage] = useState('home'); // Navigation entre pages
+  const [selectedCharacter, setSelectedCharacter] = useState(null); // Stocke le personnage sélectionné
+
+  const renderPage = () => {
+    if (currentPage === 'home') {
+      return <HomePage onNavigate={() => setCurrentPage('list')} />;
+    } else if (currentPage === 'list') {
+      return (
+        <ListPage
+          onCharacterSelect={(character) => {
+            setSelectedCharacter(character);
+            setCurrentPage('detail');
+          }}
+        />
+      );
+    } else if (currentPage === 'detail') {
+      return <DetailPage character={selectedCharacter} onNavigateBack={() => setCurrentPage('list')} />;
+    }
+  };
+
+  return <div>{renderPage()}</div>;
+}
+
+export default App;
